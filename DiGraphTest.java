b74bf9fc1185d8e00ce6cb51e9d3304b931e251e
vikash <vkdinventor@gmail.com>
2019-11-11 15:20:57 +0530

    Longest Common Subsequence

---
+++ DiGraphTest.java
@@ -0,0 +1,113 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class DiGraphTest {
+    public static void main(String[] args){
+        Digraph digraph = new Digraph(7);
+        digraph.addEdge(0,1);
+        digraph.addEdge(0,2);
+        digraph.addEdge(0,5);
+        digraph.addEdge(3,6);
+        digraph.addEdge(3,5);
+        digraph.addEdge(3,4);
+        digraph.addEdge(5,2);
+        digraph.addEdge(6,4);
+        digraph.addEdge(6,0);
+        digraph.addEdge(3,2);
+        digraph.addEdge(1,4);
+        TopologicalSort topologicalSort = new TopologicalSort(digraph);
+        topologicalSort.topologicalSort();
+    }
+}
+
+class TopologicalSort {
+    boolean[] marked;
+    Digraph digraph;
+
+    public TopologicalSort(Digraph digraph) {
+        this.digraph = digraph;
+    }
+
+    public void topologicalSort(){
+
+        Stack<Integer> reversePostOrder = new Stack<>();
+        marked = new boolean[digraph.V()];
+        for(int i=0; i< digraph.V(); i++){
+            if(!marked[i]){
+                dfs(i, digraph, marked, reversePostOrder);
+            }
+        }
+
+        while (!reversePostOrder.isEmpty()){
+            System.out.print(reversePostOrder.pop()+" ");
+        }
+
+    }
+
+    public void postOrder(int src, Digraph graph, boolean[] marked){
+        marked[src] = true;
+        for( int i : graph.adj(src)){
+            if(!marked[i]){
+                postOrder(i, graph, marked);
+            }
+        }
+        System.out.print(src+" ");
+    }
+
+    //post order dfs
+    public void dfs(int src, Digraph digraph, boolean[] marked, List<Integer> reversePostOrder){
+        marked[src] = true;
+        for( int i:  digraph.adj(src)){
+            if(!marked[i]){
+                dfs(i, digraph, marked, reversePostOrder);
+            }
+        }
+        reversePostOrder.add(src);
+    }
+}
+
+class Digraph {
+
+    //create a digraph from input stream
+    //Digraph(In in)
+    private final int V;
+    private final List<Integer>[] adj;
+
+    //create an empty digraph with V vertices
+    Digraph(int v) {
+        this.V = v;
+        adj = (List<Integer>[]) new ArrayList[V];
+        for (int i = 0; i < V; i++) {
+            adj[i] = new ArrayList<>();
+        }
+    }
+
+    //add a directed edge v→w
+    void addEdge(int v, int w) {
+        adj[v].add(w);
+    }
+
+    int V() {
+        return V;
+    }
+
+    //vertices pointing from v
+    List<Integer> adj(int v) {
+        return adj[v];
+    }
+
+    //number of edges
+    int E() {
+        return 0;
+    }
+
+    //reverse of this digraph
+    Digraph reverse() {
+        return null;
+    }
+
+    public String toString() {
+        return null;
+    }
+}
---
+++ LongestCommonSub.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class LongestCommonSub {
+    public static void main (String[] args) {
+        //code
+        String s1 = "AGGTAB";
+        String s2 = "GXTXAYB";
+        System.out.println(lcs2(s1,s2, s1.length(), s2.length()));
+        System.out.println(lcs(s1,s2, s1.length(), s2.length()));
+
+    }
+
+    public static int lcs(String s1, String s2, int l, int m){
+        if (l <= 0 || m <= 0){
+            return 0;
+        }
+
+        if( s1.charAt(l-1) == s2.charAt(m-1)){
+            return (1 + lcs (s1, s2, l-1, m-1) );
+        }else {
+            return Math.max(lcs(s1, s2, l, m-1),lcs(s1, s2, l-1, m));
+        }
+
+    }
+
+    public static int lcs2(String s1, String s2, int m, int n) {
+
+        int[][] matrix = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1))
+                    matrix[i][j] = matrix[i - 1][j - 1] + 1;
+                else
+                    matrix[i][j] = Math.max(matrix[i - 1][j], matrix[i][j - 1]);
+            }
+        }
+        return matrix[m][n];
+    }
+}
